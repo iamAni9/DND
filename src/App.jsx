c7e54a17947892afe54f5c308fafb050140a0fd8
iamAni9 <iamani7464@gmail.com>
2026-06-30 13:38:23 +0530

    fix: Vercel import issue fix for vite.

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import ReviewsSection from '@/sections/ReviewsSection'
 import ContactSection from '@/sections/ContactSection'
 import Footer from '@/sections/Footer'
 
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   return (
